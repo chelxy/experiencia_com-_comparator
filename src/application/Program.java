33cf591741b1af1d0e86b2b12a6cbb10fec740ad
Michel Morais <valentimmichel@gmail.com>
2023-03-27 11:15:43 -0300

    Comparator objeto de classe anônima

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,8 @@
-//Comparator objeto de classe separada
+//Comparator objeto de classe anônima
 package application;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import entities.Product;
@@ -15,8 +16,17 @@ public class Program {
 		list.add(new Product("TV", 900.00));
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
+		
+		//Criando uma classe anônima no programa principal
+		Comparator<Product> comp = new Comparator<Product>() {
 
-		list.sort(new MyComparator());
+			@Override
+			public int compare(Product p1, Product p2) {
+				return  p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}
+		};
+		
+		list.sort(comp);
 
 		for (Product p : list) {
 			System.out.println(p);
