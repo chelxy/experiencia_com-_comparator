e02dc07bc8bbc9dd8a49a06816bfa6129c300142
Michel Morais <valentimmichel@gmail.com>
2023-03-27 14:07:48 -0300

    Comparator objeto de expressão lambda sem chaves(arrow function)

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,10 +16,8 @@ public class Program {
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
 		
-		//Comparator objeto de expressão lambda com chaves(arrow function)
-		Comparator<Product> comp = (p1, p2) -> {
-			return  p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-		};
+		//Comparator objeto de expressão lambda sem chaves(arrow function)
+		Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 				
 		list.sort(comp);
 
